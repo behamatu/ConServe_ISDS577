544573f330a6a3627494ca555167b427dbdbe379
jorge <nzm.music@gmail.com>
2015-03-30 16:37:33 -0700

    Added Sign Up Activity Design

--- app/src/main/java/group4/isds577/conserve/SignUpActivity.java
+++ app/src/main/java/group4/isds577/conserve/SignUpActivity.java
@@ -4,28 +4,21 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.Window;
-import android.view.WindowManager;
 
 
-
-public class LoginActivity extends ActionBarActivity{
-
+public class SignUpActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_sign_up);
     }
-
-
-
 
 /*
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_login, menu);
+        getMenuInflater().inflate(R.menu.menu_sign_up, menu);
         return true;
     }
 
@@ -42,5 +35,5 @@ public class LoginActivity extends ActionBarActivity{
         }
 
         return super.onOptionsItemSelected(item);
-    }*/
+    } */
 }
