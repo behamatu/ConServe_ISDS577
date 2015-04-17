d113b5c03482864f1f743e4f3ba0f72b226561cb
jorge <nzm.music@gmail.com>
2015-04-17 13:14:55 -0700

    Login and Sign Up activities using Parse.

--- app/src/main/java/group4/isds577/conserve/ParseLoginActivity.java
+++ app/src/main/java/group4/isds577/conserve/ParseLoginActivity.java
@@ -9,7 +9,8 @@ import com.parse.Parse;
  */
 public class ParseLoginActivity extends Application{
     public void onCreate(){
-        // Initialize Parse.
+        // Initialize Parse and enable local datastore for offline data saves
+        Parse.enableLocalDatastore(this);
         Parse.initialize(this, "5j96qUWZZlWQYlXU2aShw1QDz5ttrAAqpqDl8mUk", "civ3tGjSsL3yWJWhWNJfSAkbG6VS5KcYvjki0AgX");
     }
 
